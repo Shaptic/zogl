0eeee1674504080a1f45e9e07bb92d99327b536d
George <zloi.tatarin@yahoo.com>
2014-05-26 13:06:41 -0700

    Finalizes render target object.

--- js/rendertarget.js
+++ js/rendertarget.js
@@ -4,19 +4,26 @@ zogl.zRenderTarget = function(w, h) {
     this.texture = null;
 
     this.size = {
-        'w': w,
-        'h': h
+        'w': w || glGlobals.activeWindow.size.w,
+        'h': h || glGlobals.activeWindow.size.h
     };
+
+    this.proj = mat4.create();
+    mat4.ortho(0, w, h, 0, 1.0, 10.0, this.proj);
 };
 
 zogl.zRenderTarget.prototype.init = function() {
-    this.rbo = gl.createRenderBuffer();
+    this.bind();
 
-    this.texture = zogl.zTexture();
+    this.rbo = gl.createRenderbuffer();
+
+    this.texture = new zogl.zTexture();
     this.texture.loadFromRaw(null, false, this.size.w, this.size.h);
+    this.texture.bind();
 
     gl.bindRenderbuffer(gl.RENDERBUFFER, this.rbo);
-    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, this.fbo);
+    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, 
+                           this.size.w, this.size.h);
 
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0,
                             gl.TEXTURE_2D, this.texture.id, 0);
@@ -24,14 +31,19 @@ zogl.zRenderTarget.prototype.init = function() {
                                gl.RENDERBUFFER, this.rbo);
 
     this.texture.unbind();
-    this.bindRenderbuffer(gl.RENDERBUFFER, null);
-    this.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+    this.unbind();
 };
 
 zogl.zRenderTarget.prototype.bind = function() {
     gl.bindFramebuffer(gl.FRAMEBUFFER, this.fbo);
+    gl.viewport(0, 0, this.size.w, this.size.h);
+    glGlobals.proj = this.proj;
 };
 
 zogl.zRenderTarget.prototype.unbind = function() {
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.viewport(0, 0, glGlobals.activeWindow.size.w,
+                      glGlobals.activeWindow.size.h);
+    glGlobals.proj = glGlobals.activeWindow.proj;
 };
