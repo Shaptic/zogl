2ccf0a610566fac48ed9e3a201eeb89e012d57cf
George <zloi.tatarin@yahoo.com>
2014-05-26 15:30:48 -0700

    Scene draws to internal FBO first, then screen.

--- js/scene.js
+++ js/scene.js
@@ -1,20 +1,23 @@
-zogl = zogl || {};
-
 zogl.zScene = function(w, h, options) {
     options = options || {};
 
     this.size = {
-        'w': w || glGlobals.activeWindow.size.w || 300,
-        'h': h || glGlobals.activeWindow.size.h || 300
+        'w': w || glGlobals.activeWindow.size.w,
+        'h': h || glGlobals.activeWindow.size.h
     };
 
     this.geometryVAO = new zogl.zBufferSet(gl.DYNAMIC_DRAW);
-    //this.fbo1 = new zogl.zRenderTarget();
-    //this.fbo2 = new zogl.zRenderTarget();
+    this.fbo1 = new zogl.zRenderTarget();
+    this.fbo2 = new zogl.zRenderTarget();
 
     this.lights  = [];
     this.postfx  = [];
     this.objects = [];
+
+    this.fullscreen = new zogl.zQuad(this.size.w, this.size.h);
+    this.fullscreen.attachTexture(this.fbo1.texture);
+    this.fullscreen.create();
+    //this.fullscreen.offload(this.geometryVAO, { 'preserve': false });
 
     this.flags = {
         'lighting':         options.lighting        || false,
@@ -36,18 +39,25 @@ zogl.zScene.prototype.draw = function(color) {
     for (var i in this.objects) {
         this.objects[i].offload(this.geometryVAO, { 'preserve': false });
     }
-
     this.geometryVAO.offload();
+
     this.geometryVAO.bind();
     for (var i in this.objects) {
         this.objects[i].draw(true);
     }
 
-    this.geometryVAO.unbind();
+    this.fbo1.unbind();
+    this.fullscreen.draw();
 };
 
 zogl.zScene.prototype.addObject = function() {
     var z = new zogl.zSprite();
     this.objects.push(z);
     return z;
+};
+
+zogl.zScene.prototype.addLight = function(type) {
+    var z = new zogl.zLight(type);
+    this.lights.push(z);
+    return l;
 };
