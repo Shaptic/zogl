9ecdeaa5a7c0d502c5584bb9c7baf2b099eaa6ae
Ruskiy69 <zloi.tatarin@yahoo.com>
2014-06-02 17:20:52 -0700

    Adds preliminary multi-pass API.

--- js/sprite.js
+++ js/sprite.js
@@ -1,5 +1,3 @@
-zogl = zogl || {};
-
 zogl.zSprite = function() {
     this.rect = {
         'x': 0,
@@ -8,6 +6,7 @@ zogl.zSprite = function() {
         'h': 0
     }
     this.prims = [];
+    this.passes = [];
     this.mv = mat4.create();
     this.position = vec3.create([0, 0, 0]);
     this.flags = {
@@ -18,15 +17,27 @@ zogl.zSprite = function() {
 };
 
 zogl.zSprite.prototype.loadFromTexture = function(texture) {
+    if (typeof texture == "string") {
+        texture = new zogl.zTexture();
+        texture.loadFromFile(texture);
+    }
+
     this.rect.w = texture.size.w;
     this.rect.h = texture.size.h;
 
     var q = new zogl.zQuad(this.rect.w, this.rect.h);
-    q.attachTexture(texture);
-    q.create();
 
-    this.prims = [];
-    this.addObject(q, 0, 0);
+    var tmp = texture.id.image.onload;
+    var that = this;
+    texture.id.image.onload = function() {
+        tmp();
+        q.resize(texture.size.w, texture.size.h);
+        q.attachTexture(texture);
+        q.create();
+        that.prims = [];
+        that.addObject(q, 0, 0);
+        log('from callback', q.size);
+    }
 };
 
 zogl.zSprite.prototype.addObject = function(obj, x, y) {
@@ -40,7 +51,83 @@ zogl.zSprite.prototype.move = function(x, y) {
     this.position = vec3.create([x, y, 0]);
 };
 
+zogl.zSprite.prototype.addPass = function(shader, options) {
+    this.passes.push(shader);
+};
+
 zogl.zSprite.prototype.draw = function(ready) {
+    if (this.passes.length == 0) {
+        this._drawPrims(ready);
+        return;
+    }
+
+    var wontwork = false;
+    if (this.passes.length == 1) {
+        for (var i in this.prims) {
+            if (this.prims[i].getShader() == glGlobals.defaultShader) {
+                this.prims[i].setShader(this.passes[0]);
+            } else {
+                wontwork = true;
+            }
+        }
+    }
+
+    if (!wontwork) {
+        this._drawPrims(ready);
+        return;
+    }
+
+    // We have some sort of custom shaders in the internal primitives.
+    // This means that we need to RTT.
+
+    // First, we need to calculate the total width of the internal
+    // primitives. Since some are offset, real_size = offset + size.
+
+    var mw = 0, mh = 0;
+    for (var i in this.prims) {
+        var w = this.prims[i].size.w + this.prims[i].getX();
+        var h = this.prims[i].size.h + this.prims[i].getY();
+
+        mw = mw > w ? mw : w;
+        mh = mh > h ? mh : h;
+    }
+
+    var fbo1 = new zogl.zRenderTarget(mw, mh), fbo2 = null;
+    var activeFBO = fbo1;
+    if (this.passes.length > 1) {
+        fbo2 = new zogl.zRenderTarget(mw, mh);
+    }
+
+    for (var j in this.passes) {
+        activeFBO.bind();
+        this.passes[j].bind();
+
+        this._drawPrims();
+
+        if (fbo2 !== null && fbo1 !== null) {
+            if (activeFBO == fbo1) {
+                activeFBO = fbo2;
+            } else {
+                activeFBO = fbo1;
+            }
+        }
+    }
+
+    glGlobals.defaultShader.unbind();
+
+    var q = new zogl.zQuad();
+    q.attachTexture(activeFBO.texture);
+    q.create();
+    q.draw();
+};
+
+zogl.zSprite.prototype.offload = function(vao, flags) {
+    for (var i in this.prims) {
+        this.prims[i].offload(vao, flags);
+    }
+};
+
+zogl.zSprite.prototype._drawPrims = function(ready) {
     for (var i in this.prims) {
         var pos = [
             this.prims[i].getX(),
@@ -65,11 +152,5 @@ zogl.zSprite.prototype.draw = function(ready) {
         if (this.flags.blend && !this.prims[i+1]) {
             gl.disable(gl.BLEND);
         }
-    }
-};
-
-zogl.zSprite.prototype.offload = function(vao, flags) {
-    for (var i in this.prims) {
-        this.prims[i].offload(vao, flags);
     }
 };
