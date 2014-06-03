2396423b0ae19e4433248e62b5ff48bdd9008514
Ruskiy69 <zloi.tatarin@yahoo.com>
2014-06-02 17:21:01 -0700

    Restructures code.

--- js/texture.js
+++ js/texture.js
@@ -10,16 +10,10 @@ zogl.zTexture = function() {
 };
 
 zogl.zTexture.prototype.loadFromFile = function(filename) {
+    this.loadFromRaw([255, 255, 255, 255], false, 1, 1);
+
     this.filename = filename;
-    this.id = gl.createTexture();
-
-    this.bind();
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-                  new Uint8Array([255, 255, 255, 255]));
-    this.unbind();
-
     this.id.image = new Image();
-    this.size.w = this.size.h = 1;
 
     var that = this;
     this.id.image.onload = function() {
@@ -30,7 +24,7 @@ zogl.zTexture.prototype.loadFromFile = function(filename) {
 };
 
 zogl.zTexture.prototype.loadFromRaw = function(data, flip_y, w, h) {
-    this.filename = "raw data";
+    this.filename = this.filename || "raw data";
     this.id = this.id || gl.createTexture();
     this.id.image = data;
 
@@ -44,13 +38,15 @@ zogl.zTexture.prototype.loadFromRaw = function(data, flip_y, w, h) {
         if (data instanceof Array) data = new Uint8Array(data);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, w, h, 0, gl.RGBA,
                       gl.UNSIGNED_BYTE, data);
-        this.size.w = w; this.size.h = h;
+        this.size.w = w;
+        this.size.h = h;
 
     } else {
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
                       gl.UNSIGNED_BYTE, data);
-        this.size.w = data.width  || 1;
-        this.size.h = data.height || 1;
+        this.size.w = data.width;
+        this.size.h = data.height;
+        log(this.size);
     }
 
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
