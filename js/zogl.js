d0b1ee4a9f7d49d201e2095c8c62c7ac9bf53241
George <zloi.tatarin@yahoo.com>
2014-06-08 13:04:08 -0700

    Adds a rect object that can detect collisions.

--- js/zogl.js
+++ js/zogl.js
@@ -26,6 +26,38 @@ zogl.getMousePosition = function(evt) {
     }
 };
 
+zogl.rect = function(x, y, w, h) {
+    this.x = x || 0;
+    this.y = y || 0;
+    this.w = w || 0;
+    this.h = h || 0;
+};
+
+zogl.rect.prototype.collideRect = function(rect) {
+    var top1    = this.y,
+        bottom1 = this.y + this.h,
+        right1  = this.x + this.w,
+        left1   = this.x;
+
+    var top2    = rect.y,
+        bottom2 = rect.y + rect.h,
+        right2  = rect.x + rect.w,
+        left2   = rect.x;
+
+    if (bottom1 <= top2     || 
+        top1    >= bottom2  ||
+        right1  <= left2    ||
+        left1   >= right2) {
+        return false;
+    }
+
+    return true;
+};
+
+zogl.rect.prototype.collidePosition = function(x, y) {
+    return this.collideRect(new zogl.rect(x, y, 1, 1));
+};
+
 /**
  * Accepts lots of formats for color values:
  *
